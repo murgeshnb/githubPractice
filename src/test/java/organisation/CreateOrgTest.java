82707247f6664dd34258fdea84ea5430bc705592
Dattu V Kulkarni <Dattu V Kulkarni@LAPTOP-P09V1KSC>
2022-01-26 20:48:41 +0530

    1st push from user2 for git conflict

--- src/test/java/organisation/CreateOrgTest.java
+++ src/test/java/organisation/CreateOrgTest.java
@@ -8,6 +8,8 @@ public class CreateOrgTest {
 	public void creatingOrg()
 	{
 		System.out.println("creating organisation");
+		
+		System.out.println("first attempt for git conflict from user2");
 	}
 
 }
